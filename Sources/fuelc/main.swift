89131149654d13863fb3b35c3433ff97c813519b
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-12-11 22:32:20 +0100

    Emit LLVM bytecode

--- Sources/fuelc/main.swift
+++ Sources/fuelc/main.swift
@@ -12,15 +12,16 @@ func main() throws {
   // Configure the driver's pipeline.
   driver.pipeline.append(.parse(URL(fileURLWithPath: "Hello.fuel")))
   driver.pipeline.append(.runSema)
+  driver.pipeline.append(.emitLLVM)
 
   // Execute the driver.
   try driver.execute()
 
   // Dump the compiled module.
-  if let module = driver.module {
+  if CommandLine.arguments.contains("--dump-ast") {
     let fos = FileOutputStream.stdout
     let dumper = ASTDumper(output: fos)
-    dumper.visit(module)
+    dumper.visit(driver.module)
     fos.write("\n")
   }
 }
